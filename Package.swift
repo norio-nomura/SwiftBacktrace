29b5a170699e742f587b5551ee583735610ecf82
Norio Nomura <norio.nomura@gmail.com>
2019-04-01 19:47:51 +0900

    Revert "Update dependency"
    This reverts commit cbc9bd200f021577125c85b9b5514d4d0583b1dc.

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["Clibunwind", "CSwiftBacktrace", "SwiftBacktrace"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0")
+        .package(url: "https://github.com/apple/swift-nio.git", from: "1.11.0")
     ],
     targets: [
         .target(name: "Clibunwind"),
